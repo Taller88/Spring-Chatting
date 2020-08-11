f08af8846e1025860ec417ea7c1d7c285694b6dd
JungJinwoo <48818574+Taller88@users.noreply.github.com>
2020-08-11 14:36:27 +0900

    chatting Program Login 기능

--- Chatting/src/main/java/com/jinwoo/chatting/HomeController.java
+++ Chatting/src/main/java/com/jinwoo/chatting/HomeController.java
@@ -2,7 +2,10 @@ package com.jinwoo.chatting;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,29 +14,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.jinwoo.chatting.controller.MemberController;
+import com.jinwoo.dto.MemberVO;
+import com.jinwoo.service.MemberService;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+    
+	@Inject
+	MemberService memService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) throws Exception{
+		logger.debug("???");
+		logger.info("home");
+		List<MemberVO> memList=memService.selectMember();
+		model.addAttribute("memberList",memList);
+		return "chatting/index";
 	}
 	
 }
--- Chatting/src/main/webapp/chatting/index.jsp
+++ Chatting/src/main/webapp/chatting/index.jsp
@@ -9,13 +9,19 @@
 <body>
 	<table>
 		<thead>
-			<td>
-				작성자
-			</td>
-			<td>
-				내용
-			</td>
+			<th>번호</th>
+			<th>아이디</th>
+			<th>이름</th>
 		</thead>
+		<tbody>
+			<c:forEach items="${memberList}" var="member">
+				<tr>
+					<td>${member.no}</td>
+					<td>${member.user_id}</td>
+					<td>${member.name}</td>
+				</tr>
+			</c:forEach>
+		</tbody>
 		<tr>
 			<td>
 				<input type="text">
